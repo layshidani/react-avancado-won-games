6d28153baf8dbd6ea80da9db03f41a224f0980be
Lays Marie Hidani <lays.hidani@gmail.com>
2020-11-26 21:41:23 -0300

    config(strapi): initial config :wrench:

---
+++ reactavancado-api/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'af4174ba32a63acc0c1f3384acfcad54'),
+    },
+  },
+});
---
+++ reactavancado-api/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '5ae485b2-0ad8-439d-b06f-28b366182d1c'
+};
