c470ab7a087416a5d35fc561d28aa8cea01a1a95
Lays Marie Hidani <lays.hidani@gmail.com>
2020-11-28 18:34:30 -0300

    config

--- my-project/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '80e264de657423d64b8ab04b136b21aa'),
-    },
-  },
-});
